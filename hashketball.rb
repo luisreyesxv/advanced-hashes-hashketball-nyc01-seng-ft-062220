5db6b338344cb6d6a21a6cf7559bc8ded514590c
Luis Reyes <luisreyesxv@yahoo.com>
2020-06-16 05:19:01 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -166,8 +166,18 @@ def most_points_scored
 end
 
 def long_name_steals_a_ton
-
-  search_most_1_symbol_metric(:steals,:player_name)
+  answer= Hash.new
+   game_hash.each do |home_or_away, team_data|
+    team_data[:players].each do |player_data|
+      if answer.empty?
+        answer[:player_name] = player_data[:player_name]
+      end
+      if player_data[:player_name].length > answer[:player_name].length
+        answer[:player_name] = player_data[:player_name]
+      end
+    end
+  end
+  answer[:player_name] == search_most_1_symbol_metric(:steals,:player_name)
 
 #helper methods to help me specify some information
 def search_player_stats(name_of_player)
@@ -213,19 +223,4 @@ def search_most_1_symbol_metric (metric_searched_by,result)
     end
   end
   answer[result]
-end
-
-
-answer= Hash.new
- game_hash.each do |home_or_away, team_data|
-  team_data[:players].each do |player_data|
-    if answer.empty?
-      answer[:player_name] = player_data[:player_name]
-    end
-    if player_data[:player_name].length > answer[:player_name].length
-      answer[:player_name] = player_data[:player_name]
-    end
-  end
-end
-answer[:player_name]
 end
