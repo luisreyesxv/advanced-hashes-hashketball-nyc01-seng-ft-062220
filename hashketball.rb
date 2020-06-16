c2724be39d2fb84f353d7a3fc71e94275fa21f41
Luis Reyes <luisreyesxv@yahoo.com>
2020-06-16 03:56:24 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -142,7 +142,7 @@ end
 
 def team_names
 
-  game_hash.each_with_object([]) {|(key,value),teams|  teams.push(value[:team_names]) }
+  game_hash.each_with_object([]) {|(key,value),teams|  value[:team_names]) }
 end
  binding.pry
 
