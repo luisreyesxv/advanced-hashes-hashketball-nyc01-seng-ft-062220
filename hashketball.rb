2d07a6e03496072a6b53671cfa823bb18a1c985e
Luis Reyes <luisreyesxv@yahoo.com>
2020-06-16 04:46:01 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -160,7 +160,6 @@ end
 def big_shoe_rebounds
   game_hash.each do |home_or_away, team_data|
     team_data[:players].each_with_object({}) do |player_data,biggest_shoe|
-      binding.pry
       if biggest_shoe.empty?
         biggest_shoe[:player_name] = player_data[:player_name]
         biggest_shoe[:shoe] = player_data[:shoe]
