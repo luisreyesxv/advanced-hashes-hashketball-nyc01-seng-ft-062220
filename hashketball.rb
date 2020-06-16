281e34aa2c8c643d469975b42ceaa2db51120b7f
Luis Reyes <luisreyesxv@yahoo.com>
2020-06-16 03:40:27 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -170,6 +170,7 @@ end
 
 
 def search_teams(team_name)
+  binding.pry
   if game_hash.any?{|i| i[:team_name]==team_name}
     game_hash[:home][:team_name]==team_name? game_hash[:home] : game_hash[:away]
   else
