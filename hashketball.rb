659a844b917258c2b10d96a31de6ef4e2bf24f16
Luis Reyes <luisreyesxv@yahoo.com>
2020-06-16 03:37:31 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -173,5 +173,5 @@ def search_teams(team_name)
   if game_hash.any{|i| i[:team_name]==team_name}
     game_hash[:home][:team_name]==team_name? game_hash[:home] : game_hash[:away]
   else
-    return "Sorry, but the team doesn't exist yet."  
-end  
+    return "Sorry, but the team doesn't exist yet."
+end
