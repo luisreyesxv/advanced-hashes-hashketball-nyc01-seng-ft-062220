0d8e6ebb5ba595492f6d22a31f9b80985ddeb513
Luis Reyes <luisreyesxv@yahoo.com>
2020-06-16 02:53:10 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -144,7 +144,7 @@ def search_player_stats(name_of_player)
 game_hash.find do |key,value|
   binding.pry
 
-  #key[:players][player_index][:player_name]==name_of_player}
+  game_hash[key][:players][player_index][:player_name]==name_of_player}
 
 end
   #game_hash.select do |home_or_away_key,team_data|
